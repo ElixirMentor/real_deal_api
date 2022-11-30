14a24d8553256c53298f314c901ab14016c668b0
Jacob Luetzow <jacob@perfectform.io>
2022-11-29 17:25:13 -0700

    schema json creation

---
+++ lib/real_deal_api/users/user.ex
@@ -0,0 +1,22 @@
+defmodule RealDealApi.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :biography, :string
+    field :full_name, :string
+    field :gender, :string
+    belongs_to :account, RealDealApi.Accounts.Account
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:account_id, :full_name, :gender, :biography])
+    |> validate_required([:account_id])
+  end
+end
---
+++ lib/real_deal_api_web/views/user_view.ex
@@ -0,0 +1,21 @@
+defmodule RealDealApiWeb.UserView do
+  use RealDealApiWeb, :view
+  alias RealDealApiWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{
+      id: user.id,
+      full_name: user.full_name,
+      gender: user.gender,
+      biography: user.biography
+    }
+  end
+end
