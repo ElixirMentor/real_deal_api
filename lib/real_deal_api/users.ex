278d6b05bf0a314c2739195a6713176af591f9d2
Jacob Luetzow <jacob@perfectform.io>
2022-12-16 15:51:07 -0700

    sign up

--- lib/real_deal_api/users.ex
+++ lib/real_deal_api/users.ex
@@ -49,8 +49,9 @@ defmodule RealDealApi.Users do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_user(attrs \\ %{}) do
-    %User{}
+  def create_user(account, attrs \\ %{}) do
+    account
+    |> Ecto.build_assoc(:user)
     |> User.changeset(attrs)
     |> Repo.insert()
   end
--- lib/real_deal_api_web/controllers/account_controller.ex
+++ lib/real_deal_api_web/controllers/account_controller.ex
@@ -1,8 +1,8 @@
 defmodule RealDealApiWeb.AccountController do
   use RealDealApiWeb, :controller
 
-  alias RealDealApi.Accounts
-  alias RealDealApi.Accounts.Account
+  alias RealDealApiWeb.Auth.Guardian
+  alias RealDealApi.{Accounts, Accounts.Account, Users, Users.User}
 
   action_fallback RealDealApiWeb.FallbackController
 
@@ -12,10 +12,12 @@ defmodule RealDealApiWeb.AccountController do
   end
 
   def create(conn, %{"account" => account_params}) do
-    with {:ok, %Account{} = account} <- Accounts.create_account(account_params) do
+    with {:ok, %Account{} = account} <- Accounts.create_account(account_params),
+          {:ok, token, _claims} <- Guardian.encode_and_sign(account),
+          {:ok, %User{} = _user} <- Users.create_user(account, account_params) do
       conn
       |> put_status(:created)
-      |> render("show.json", account: account)
+      |> render("account_token.json", %{account: account, token: token})
     end
   end
 
