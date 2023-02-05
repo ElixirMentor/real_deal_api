3643ee76d9b73317682d3a4f6d1df250e2c7694a
Jacob Luetzow <75959947+jacobluetzow@users.noreply.github.com>
2023-02-05 09:33:34 -0600

    Refesh session (#9)
    * guardian_db
    
    * refresh
    
    ---------
    
    Co-authored-by: Jacob Luetzow <jacob@perfectform.io>

--- lib/real_deal_api_web/auth/error_response.ex
+++ lib/real_deal_api_web/auth/error_response.ex
@@ -5,3 +5,7 @@ end
 defmodule RealDealApiWeb.Auth.ErrorResponse.Forbidden do
   defexception [message: "You do not have access to this resource.", plug_status: 403]
 end
+
+defmodule RealDealApiWeb.Auth.ErrorResponse.NotFound do
+  defexception [message: "Not Found", plug_status: 404]
+end
--- lib/real_deal_api_web/auth/guardian.ex
+++ lib/real_deal_api_web/auth/guardian.ex
@@ -55,6 +55,12 @@ defmodule RealDealApiWeb.Auth.Guardian do
     end
   end
 
+  def on_refresh({old_token, old_claims}, {new_token, new_claims}, _options) do
+    with {:ok, _, _} <- Guardian.DB.on_refresh({old_token, old_claims}, {new_token, new_claims}) do
+      {:ok, {old_token, old_claims}, {new_token, new_claims}}
+    end
+  end
+
   def on_revoke(claims, token, _options) do
     with {:ok, _} <- Guardian.DB.on_revoke(claims, token) do
       {:ok, claims}
