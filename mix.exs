84562c4ae79f5a76e7746b5cc6552453e28ff8d5
Jacob Luetzow <jacob@perfectform.io>
2023-02-09 08:23:56 -0700

    elixir 1.13 or >

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RealDealApi.MixProject do
     [
       app: :real_deal_api,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
