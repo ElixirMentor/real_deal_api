6d94ea4d54c2a90a6d466edecdc17af298fd70d0
Jacob Luetzow <75959947+jacobluetzow@users.noreply.github.com>
2023-02-19 13:28:39 -0600

    expiration, type, and refactor (#11)
    Co-authored-by: Jacob Luetzow <jacob@perfectform.io>

--- lib/real_deal_api_web/auth/guardian.ex
+++ lib/real_deal_api_web/auth/guardian.ex
@@ -27,10 +27,19 @@ defmodule RealDealApiWeb.Auth.Guardian do
       nil -> {:error, :unauthored}
       account ->
         case validate_password(password, account.hash_password) do
-          true -> create_token(account)
+          true -> create_token(account, :access)
           false -> {:error, :unauthorized}
         end
 
+    end
+  end
+
+  def authenticate(token) do
+    with {:ok, claims} <- decode_and_verify(token),
+         {:ok, account} <- resource_from_claims(claims),
+         {:ok, _old, {new_token, _claims}} <- refresh(token) do
+
+      {:ok, account, new_token}
     end
   end
 
@@ -38,9 +47,17 @@ defmodule RealDealApiWeb.Auth.Guardian do
     Bcrypt.verify_pass(password, hash_password)
   end
 
-  defp create_token(account) do
-    {:ok, token, _claims} = encode_and_sign(account)
+  defp create_token(account, type) do
+    {:ok, token, _claims} = encode_and_sign(account, %{}, token_options(type))
     {:ok, account, token}
+  end
+
+  defp token_options(type) do
+    case type do
+      :access -> [token_type: "access", ttl: {2, :hour}]
+      :reset -> [token_type: "reset", ttl: {15, :minute}]
+      :admin -> [token_type: "admin", ttl: {90, :day}]
+    end
   end
 
   def after_encode_and_sign(resource, claims, token, _options) do
