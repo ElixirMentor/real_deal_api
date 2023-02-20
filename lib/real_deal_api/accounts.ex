52b91c3fb087c132927cbdf60bb170e9a9cd6f02
Jacob Luetzow <75959947+jacobluetzow@users.noreply.github.com>
2023-02-20 14:04:15 -0600

    13 ecto preload user (#13)
    * ecto preload user
    
    * added .vscode to gitignore
    
    ---------
    
    Co-authored-by: Jacob Luetzow <jacob@perfectform.io>

--- lib/real_deal_api/accounts.ex
+++ lib/real_deal_api/accounts.ex
@@ -37,6 +37,13 @@ defmodule RealDealApi.Accounts do
   """
   def get_account!(id), do: Repo.get!(Account, id)
 
+  def get_full_account(id) do
+    Account
+    |> where(id: ^id)
+    |> preload([:user])
+    |> Repo.one()
+  end
+
   @doc """
   Gets a single account.any()
 
--- lib/real_deal_api_web/controllers/account_controller 2.ex
+++ lib/real_deal_api_web/controllers/account_controller 2.ex
@@ -11,7 +11,6 @@ defmodule RealDealApiWeb.AccountController do
   defp is_authorized_account(conn, _opts) do
     %{params: %{"account" => params}} = conn
     account = Accounts.get_account!(params["id"])
-
     if conn.assigns.account.id == account.id do
       conn
     else
@@ -26,31 +25,59 @@ defmodule RealDealApiWeb.AccountController do
 
   def create(conn, %{"account" => account_params}) do
     with {:ok, %Account{} = account} <- Accounts.create_account(account_params),
-         {:ok, %User{} = _user} <- Users.create_user(account, account_params) do
-      authorize_account(conn, account.email, account_params["hash_password"])
+          {:ok, token, _claims} <- Guardian.encode_and_sign(account),
+          {:ok, %User{} = _user} <- Users.create_user(account, account_params) do
+      conn
+      |> put_status(:created)
+      |> render("account_token.json", %{account: account, token: token})
     end
   end
 
   def sign_in(conn, %{"email" => email, "hash_password" => hash_password}) do
-    authorize_account(conn, email, hash_password)
-  end
-
-  defp authorize_account(conn, email, hash_password) do
     case Guardian.authenticate(email, hash_password) do
       {:ok, account, token} ->
         conn
         |> Plug.Conn.put_session(:account_id, account.id)
         |> put_status(:ok)
         |> render("account_token.json", %{account: account, token: token})
-
-      {:error, :unauthorized} ->
-        raise ErrorResponse.Unauthorized, message: "Email or Password incorrect."
+      {:error, :unauthorized} -> raise ErrorResponse.Unauthorized, message: "Email or Password incorrect."
     end
   end
 
+  # Left this commented code to match video, but refactored below for readability
+  # def refresh_session(conn, %{}) do
+  #   old_token = Guardian.Plug.current_token(conn)
+  #   case Guardian.decode_and_verify(old_token) do
+  #     {:ok, claims} ->
+  #       case Guardian.resource_from_claims(claims) do
+  #         {:ok, account} ->
+  #           {:ok, _old, {new_token, _new_claims}} = Guardian.refresh(old_token)
+  #           conn
+  #           |> Plug.Conn.put_session(:account_id, account.id)
+  #           |> put_status(:ok)
+  #           |> render("account_token.json", %{account: account, token: new_token})
+  #         {:error, _reason} ->
+  #           raise ErrorResponse.NotFound
+  #       end
+  #     {:error, _reason} ->
+  #       raise ErrorResponse.NotFound
+  #   end
+  # end
+
+  @doc """
+  Removed case statements and added multiple function clauses with pattern matching for readability.
+  """
   def refresh_session(conn, %{}) do
-    token = Guardian.Plug.current_token(conn)
-    {:ok, account, new_token} = Guardian.authenticate(token)
+    old_token = Guardian.Plug.current_token(conn)
+
+    claims = Guardian.decode_and_verify(old_token)
+    |> refresh_session_validation()
+
+    account = Guardian.resource_from_claims(claims)
+    |> refresh_session_validation()
+
+    new_token = Guardian.refresh(old_token)
+    |> refresh_session_validation()
 
     conn
     |> Plug.Conn.put_session(:account_id, account.id)
@@ -58,11 +85,16 @@ defmodule RealDealApiWeb.AccountController do
     |> render("account_token.json", %{account: account, token: new_token})
   end
 
+  defp refresh_session_validation({:ok, value}), do: value
+
+  defp refresh_session_validation({:ok, _old, {value, _claims}}), do: value
+
+  defp refresh_session_validation({:error, _reason}), do: raise ErrorResponse.NotFound
+
   def sign_out(conn, %{}) do
     account = conn.assigns[:account]
     token = Guardian.Plug.current_token(conn)
     Guardian.revoke(token)
-
     conn
     |> Plug.Conn.clear_session()
     |> put_status(:ok)
@@ -70,8 +102,8 @@ defmodule RealDealApiWeb.AccountController do
   end
 
   def show(conn, %{"id" => id}) do
-    account = Accounts.get_full_account(id)
-    render(conn, "full_account.json", account: account)
+    account = Accounts.get_account!(id)
+    render(conn, "show.json", account: account)
   end
 
   def update(conn, %{"account" => account_params}) do
