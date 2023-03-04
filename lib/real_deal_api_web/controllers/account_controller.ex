9768fce4d2b58e9fa16ff0a33e438b64902e022d
Jacob Luetzow <jacob@perfectform.io>
2023-03-04 12:13:30 -0700

    change account with password

--- lib/real_deal_api_web/controllers/account_controller.ex
+++ lib/real_deal_api_web/controllers/account_controller.ex
@@ -65,11 +65,20 @@ defmodule RealDealApiWeb.AccountController do
     render(conn, "full_account.json", account: account)
   end
 
-  def update(conn, %{"account" => account_params}) do
-    account = Accounts.get_account!(account_params["id"])
+  def current_account(conn, %{}) do
+    conn
+    |> put_status(:ok)
+    |> render("full_account.json", %{account: conn.assigns.account})
+  end
 
-    with {:ok, %Account{} = account} <- Accounts.update_account(account, account_params) do
-      render(conn, "show.json", account: account)
+  def update(conn, %{"current_hash" => current_hash, "account" => account_params}) do
+    case Guardian.validate_password(current_hash, conn.assigns.account.hash_password) do
+      true ->
+        {:ok, account} = Accounts.update_account(conn.assigns.account, account_params)
+        render(conn, "show.json", account: account)
+
+      false ->
+        raise ErrorResponse.Unauthorized, message: "Password incorrect."
     end
   end
 
