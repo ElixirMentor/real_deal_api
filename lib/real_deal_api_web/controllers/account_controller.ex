27cb14996a04f4c1b13180484d0e64461f1b0176
Jacob Luetzow <jacob@perfectform.io>
2023-03-08 13:27:31 -0700

    verify password to update account data

--- lib/real_deal_api_web/controllers/account_controller.ex
+++ lib/real_deal_api_web/controllers/account_controller.ex
@@ -6,7 +6,7 @@ defmodule RealDealApiWeb.AccountController do
 
   import RealDealApiWeb.Auth.AuthorizedPlug
 
-  plug :is_authorized when action in [:update, :delete]
+  plug :is_authorized, %{resource: :account} when action in [:update, :delete]
 
   action_fallback RealDealApiWeb.FallbackController
 
